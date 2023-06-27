afa8ff4eebe82a610814fe71e3bcd2370a6d4d8a
Uliana <uosintseva@gmail.com>
2023-06-27 19:19:29 +0300

    add files

---
+++ src/test/java/MoneyTransferPage.java
@@ -0,0 +1,84 @@
+package ru.netology.web.test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.web.data.DataHelper;
+import ru.netology.web.page.DashboardPage;
+import ru.netology.web.page.LoginPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MoneyTransferTest {
+
+    @BeforeEach
+    public void openPage() {
+
+        open( "http://localhost:7777" );
+
+        var loginPage = new LoginPage();
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin( authInfo );
+        var verificationCode = DataHelper.getVerificationCodeFor( authInfo );
+        verificationPage.validVerify( verificationCode );
+    }
+
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards1() {
+
+        var dashboardPage = new DashboardPage();
+
+        int balanceFirstCard = dashboardPage.getFirstCardBalance();
+        int balanceSecondCard = dashboardPage.getSecondCardBalance();
+        var moneyTransfer = dashboardPage.firstCardButton();
+        var infoCard = DataHelper.getSecondCardNumber();
+        String sum = "100";
+        moneyTransfer.transferForm( sum, infoCard );
+
+        assertEquals( balanceFirstCard + Integer.parseInt( sum ), dashboardPage.getFirstCardBalance() );
+        assertEquals( balanceSecondCard - Integer.parseInt( sum ), dashboardPage.getSecondCardBalance() );
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCards2() {
+
+        var dashboardPage = new DashboardPage();
+
+        int balanceFirstCard = dashboardPage.getFirstCardBalance();
+        int balanceSecondCard = dashboardPage.getSecondCardBalance();
+        var moneyTransfer = dashboardPage.secondCardButton();
+        var infoCard = DataHelper.getFirstCardNumber();
+        String sum = "1500";
+        moneyTransfer.transferForm( sum, infoCard );
+
+        assertEquals( balanceFirstCard - Integer.parseInt( sum ), dashboardPage.getFirstCardBalance() );
+        assertEquals( balanceSecondCard + Integer.parseInt( sum ), dashboardPage.getSecondCardBalance() );
+    }
+
+    @Test
+    void shouldCancellationOfMoneyTransfer() {
+
+        var dashboardPage = new DashboardPage();
+
+        var moneyTransfer = dashboardPage.firstCardButton();
+        moneyTransfer.cancelButton();
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsError() {
+
+        var dashboardPage = new DashboardPage();
+
+        var moneyTransfer = dashboardPage.secondCardButton();
+        var infoCard = DataHelper.getFirstCardNumber();
+        String sum = "20000";
+        moneyTransfer.transferForm( sum, infoCard );
+        moneyTransfer.getError();
+    }
+}
+
+
+
+
+
